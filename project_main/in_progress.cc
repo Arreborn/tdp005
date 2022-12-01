9bde9d60108823cd5d3506dd3a3d002f5a1d347b
Love Arreborn <lovar063@student.liu.se>
2022-12-01 14:59:41 +0100

    pushing in progress

--- project_main/in_progress.cc
+++ project_main/in_progress.cc
@@ -1,45 +1,29 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Sleep.hpp>
-#include <SFML/System/Time.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/System/Vector2.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include <SFML/Window/VideoMode.hpp>
-#include <cmath>
+#include "common.h"
+
+const size_t width = 1280;
+const size_t height = 800;
 
 int main() {
+    sf::RenderWindow window{sf::VideoMode{width, height}, "Gaming Mode: Enbled"};
+    window.setKeyRepeatEnabled(false);
+    window.setVerticalSyncEnabled(true);
+    bool is_running{true};
 
-  // windows in SFML are handled like instances of a class, where this is the first instance of said class
-  sf::RenderWindow window{sf::VideoMode(1280, 800), "Hello World!"};
+    sf::Event event{};
+    while(window.waitEvent(event)){
 
-  // keeps window open for five seconds
-  // sf::sleep(sf::seconds(5));
+      switch (event.type){
+        case sf::Event::Closed:
+          is_running = false;
+          break;
+          
+        default:
+          break;
 
-  bool is_running{true};
-  sf::Event event;
-  sf::CircleShape circle{40};
-  circle.setPosition(sf::Vector2f{300, 300});
-  sf::Clock clock;
+      }
 
-  //runs a continuous loop
-  while (window.waitEvent(event)) {
-    // checks the event types
-    switch (event.type) {
-    // closes program is the window is closed
-    case sf::Event::Closed:
-      is_running = false;
-      break;
-      
-    default:
-      break;
+      if (!is_running) { break; }
     }
-      
-    if (!is_running) { break; }
-  }
 
-  
-  return 0;
+    return 0;
 }
