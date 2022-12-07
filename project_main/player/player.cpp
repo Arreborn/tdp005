18a25b0b0943a6876bcd3342c04df28ffc7f708d
Love Arreborn <lovar063@student.liu.se>
2022-12-07 10:52:16 +0100

    jump working

--- project_main/player/player.cpp
+++ project_main/player/player.cpp
@@ -6,43 +6,44 @@
 bool canMove{true};
 
 Player::Player(sf::Vector2f center)
-    : Entity(center, "sprites/warrior1_new.png"), health{10}, speed{400.0},
-      type{'P'} {
+    : Entity(center, "sprites/warrior1_new.png"), health{10}, speed{100.0},
+      type{'P'}, isJumping{true} {
   shape.setOutlineColor(sf::Color::Blue);
   shape.setOutlineThickness(0);
 }
 
-sf::Vector2f horizontal_position() {
-  sf::Vector2f position{0, 1};
+sf::Vector2f horizontal_position(sf::Vector2f &acceleration, bool &isJumping) {
+  if (isJumping && acceleration.y < 10){
+    ++acceleration.y;
+  }
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { // this must handle jumps
-    position.y -= 1;
-  }
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-    position.y += 1;
-  }
-    return position;
+      if (!isJumping){
+        isJumping = true;
+        acceleration.y -= 10;
+      }
+  }  //position.y -= 1;
+
+  return acceleration;
 }
 
 sf::Vector2f vertical_position(){
   sf::Vector2f position{};
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-    position.x -= 1;
+    position.x -= 3.6;
   }
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-    position.x += 1;
+    position.x += 3.6;
   }
   return position;
 }
 
 bool Player::tick(sf::Time time, World &world) {
   sf::Vector2f hold{center};
-  sf::Vector2f vold(center);
   // if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) {
   //   Attack::attackMelee();
   // }
 
-  auto hdir{horizontal_position()};
-
+  auto hdir{horizontal_position(acceleration, isJumping)};
   center += hdir * (speed * time.asMicroseconds() / 1000000.0f);
   shape.setPosition(center);
 
@@ -50,18 +51,18 @@ bool Player::tick(sf::Time time, World &world) {
     if (dynamic_cast<Block *>(collision.get())) {
       center = hold;
       shape.setPosition(hold);
+      isJumping = false;
     }
   }
-
+  sf::Vector2f vold(center);
   auto vdir(vertical_position());
-
   center += vdir * (speed * time.asMicroseconds() / 1000000.0f);
-  shape.setPosition(center);
+  sprite.setPosition(center);
 
     for (auto &collision : world.collidesWith(*this)) {
     if (dynamic_cast<Block *>(collision.get())) {
       center = vold;
-      shape.setPosition(vold);
+      sprite.setPosition(vold);
     }
   }
 
