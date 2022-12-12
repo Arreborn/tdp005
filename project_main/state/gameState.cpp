a981429eb14a5a3ce5239c08022ef7a9e06af9ba
Elliot Hernesten <ellhe126@student.liu.se>
2022-12-12 16:12:37 +0100

    Experimenting with hpbar

--- project_main/state/gameState.cpp
+++ project_main/state/gameState.cpp
@@ -1,40 +1,58 @@
 #include "gameState.h"
 #include "../level/levelConstructor.h"
 #include "../player/player.h"
+#include "../sprites/spriteManager.h"
 #include "menuState.h"
 #include <SFML/Graphics/View.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics.hpp>
 #include <memory>
 
-GameState::GameState() {
+GameState::GameState()
+{
+  sf::Texture *t = SpriteManager::get("sprites/Healthbar.png");
+  auto size = t->getSize();
+  hpBar.setTexture(*t);
+  hpBar.setOrigin(size.x, size.y / 2.0f);
+
   LevelConstructor::generateLevel(world);
   auto player{std::make_shared<Player>(sf::Vector2f(width / 2.0, height / 2.0))};
   world.add(player);
 }
 
-shared_ptr<State> GameState::tick(sf::Time time) {
+shared_ptr<State> GameState::tick(sf::Time time)
+{
+  if (player.isHit)
+  {
+    hpBar.setTextureRect(sf::IntRect(0, 0, 54, 8));
+  }
+
   // zooms camera when in gamestate
   view.setCenter(world.getCenter());
   view.setSize(640.0f, 400.0f);
   world.tick(time);
-  
 
   // Checks for pause
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+  {
     // resets viewport
     view.setCenter(640.0f, 400.0f);
     view.setSize(1280.0, 800.0);
     return std::make_shared<MenuState>(shared_from_this());
   }
 
-  if (!world.isPlayerAlive()) {
+  if (!world.isPlayerAlive())
+  {
     // return GameOverState ptr
   }
 
   return nullptr;
 }
 
-void GameState::render(sf::RenderWindow &drawTo) {
+void GameState::render(sf::RenderWindow &drawTo)
+{
   world.render(drawTo);
   drawTo.setView(view);
+  hpBar.setPosition(sf::Vector2f(640.0f, 400.0f));
+  drawTo.draw(hpBar);
 };
--- project_main/state/gameState.h
+++ project_main/state/gameState.h
@@ -2,33 +2,37 @@
 #include "state.h"
 #include "../world.h"
 
-class GameState : public State {
+class GameState : public State
+{
 public:
   /**
    * @brief Construct a new Game State object.
-   * 
+   *
    */
   GameState();
 
   /**
    * @brief Ticks all game elements.
-   * 
-   * @param delta 
-   * @return shared_ptr<State> 
+   *
+   * @param delta
+   * @return shared_ptr<State>
    */
   shared_ptr<State> tick(sf::Time delta) override;
 
   /**
    * @brief Draws all game elements.
-   * 
-   * @param drawTo 
+   *
+   * @param drawTo
    */
   void render(sf::RenderWindow &drawTo) override;
+
+protected:
+  sf::Sprite hpBar;
 
 private:
   /**
    * @brief Stores the game world.
-   * 
+   *
    */
   World world;
 };
