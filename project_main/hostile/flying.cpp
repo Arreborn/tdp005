f56919c1435e802ae7c7d50e97ccb2d6b7c8ed86
Love Arreborn <lovar063@student.liu.se>
2022-12-15 11:50:23 +0100

    render override removed

--- project_main/hostile/flying.cpp
+++ project_main/hostile/flying.cpp
@@ -18,20 +18,10 @@ bool Flying::tick(sf::Time time, World &world) {
   return Hostile::tick(time, world);
 }
 
-<<<<<<< HEAD
-void Flying::verticalPosition(sf::Time const &time, World &world)
-{
-    bool yMax{};
-    sf::Vector2f playerPos{world.playerCharacter->getCenter()};
-    // sf::Vector2f vold{center};
-=======
-void Flying::render(sf::RenderWindow &drawTo) { Hostile::render(drawTo); }
-
 void Flying::verticalPosition(sf::Time const &time, World &world) {
   bool yMax{};
   sf::Vector2f playerPos{world.playerCharacter->getCenter()};
   // sf::Vector2f vold{center};
->>>>>>> f0309808e4838badf6abda380510e4d91f9281a0
 
   cout << "Player y: " << playerPos.y << " enemy: " << center.y << endl;
 
@@ -72,69 +62,6 @@ void Flying::verticalPosition(sf::Time const &time, World &world) {
   }
 }
 
-<<<<<<< HEAD
-void Flying::horizontalPosition(sf::Time const &time, World &world)
-{
-    sf::Vector2f playerPos{world.playerCharacter->getCenter()};
-    cout << "Player x: " << playerPos.x << " enemy: " << center.x << endl;
-    if (attackCooldown == sf::seconds(0))
-    {
-        if (playerPos.x == center.x + 10)
-        {
-            if (playerPos.x < center.x)
-            {
-                direction = 'l';
-                center.x -= 1;
-            }
-            else
-            {
-                direction = 'r';
-                center.x += 1;
-            }
-        }
-    }
-    else
-    {
-        int random{rand() % 100 + 1};
-        if (random == 100)
-        {
-            direction = 'l';
-            movementDuration += sf::seconds(0.2);
-        }
-        else if (random == 1)
-        {
-            direction = 'r';
-            movementDuration += sf::seconds(0.2);
-        }
-
-        if (movementDuration < sf::seconds(0))
-        {
-            movementDuration = sf::seconds(0);
-        }
-        else if (direction == 'l' && movementDuration > sf::seconds(0))
-        {
-            center.x -= 1;
-            movementDuration -= time;
-        }
-        else if (direction == 'r' && movementDuration > sf::seconds(0))
-        {
-            center.x += 1;
-            movementDuration -= time;
-        }
-    }
-}
-
-void Flying::attack(World &world)
-{
-    if (attackCooldown == sf::seconds(0.0f) && (world.playerCharacter->getCenter().x > center.x - 20 || world.playerCharacter->getCenter().x < center.x + 20) &&
-        world.playerCharacter->getCenter().y >= center.y + 75)
-    {
-        cout << "can shoot" << endl;
-        world.add(std::make_shared<RangedAttack>(center, 2, ptrGet()));
-        attackCooldown = sf::seconds(4.0f);
-    }
-}
-=======
 void Flying::horizontalPosition(sf::Time const &time, World &) {
 
   int random{rand() % 100 + 1};
@@ -158,4 +85,3 @@ void Flying::horizontalPosition(sf::Time const &time, World &) {
 }
 
 void Flying::attack(World &world) {}
->>>>>>> f0309808e4838badf6abda380510e4d91f9281a0
